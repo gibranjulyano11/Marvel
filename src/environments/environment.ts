4e54b0abcdcbd7d142a7ed20f1db838d91fb1c29
gibranjulyano11 <gjv.guzman11@gmail.com>
2022-11-29 12:17:33 -0500

    Auth con Firebase y Front-End consumiendo el API de Marvel

---
+++ src/environments/environment.ts
@@ -0,0 +1,29 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyBvf67US9M4GppcWDINFqbMvZKneGkX-bE',
+    authDomain: 'marvel-avengers-73dc9.firebaseapp.com',
+    projectId: 'marvel-avengers-73dc9',
+    storageBucket: 'marvel-avengers-73dc9.appspot.com',
+    messagingSenderId: '131128349304',
+    appId: '1:131128349304:web:08e6d18b3246f58fbed38d',
+  },
+
+  apiMarvel:'https://gateway.marvel.com:443/v1/public',
+  public_key: '61ef59ef3f5757c46ac2116c39c73fde',
+  hash:'a9ac12d729455027c6473f0875c10fdc',
+
+  production: false,
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
